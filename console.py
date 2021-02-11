11e545c26686b46891d412260714b2b7a47b76bb
Ethan-23 <emanroberts@gmail.com>
2021-02-11 19:12:07 +0000

    Updated class with exit and EOF commands

--- console.py
+++ console.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
 """Console"""
+import cmd
 
 
 class HBNBCommand(cmd.Cmd):
     """console for Airbnb clone"""
-    
+    prompt = '(hbnb)'
+    intro = None
+    file = None
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        return True
+    def emptyline(self):
+        return None
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
