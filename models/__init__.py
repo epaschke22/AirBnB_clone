f9023967ce48e53d4716cf0f31e36b49d706cc98
epaschke22 <ericksonpaschke@gmail.com>
2021-02-13 20:18:11 +0000

    updating base model, filestorage and models/init updating for task 5, still work to do to test if all code works

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""calls FileStorage"""
+import models.engine.file_storage
+storage = FileStorage()
+storage.reload()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,29 @@
 #!/usr/bin/python3
 """File Storage"""
+import json
 
 
 class FileStorage:
     """engie to store and load files"""
-    
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """returns all the contents of dictionary __objects"""
+        return __objects
+
+    def new(self, obj):
+        """adds new object to the __object dict"""
+        string = obj.__class__ + '.' + obj.id
+        __objects[string] = obj.to_dict
+
+    def save(self):
+        """serializes or saves the __objects dict into a json file"""
+        with open(__file_path, 'w') as file:
+            file.write(json.dumps(__objects))
+
+    def reload(self):
+        """turns the json file back into the __objects dict"""
+        try:
+            with open(__file_path) as file:
+                __objects = json.loads(file.read())
