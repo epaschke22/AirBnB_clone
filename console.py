61f644b8f4b906a5686a4d67f22a4dda36670da0
Ethan-23 <emanroberts@gmail.com>
2021-02-14 04:43:57 +0000

    updated just to hold console

--- console.py
+++ console.py
@@ -15,6 +15,37 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, arg):
         """EOF command to exit the program"""
         return True
+    def do_create(self, arg):
+        """ Creates a new instance of BaseModel, saves it (to the JSON file) and
+        prints the id."""
+        if len(arg) < 2:
+            print ("** class name missing **")
+        else:
+            return None
+    def do_show(self, arg):
+        """Prints the string representation of an
+        instance based on the class name and id"""
+        if len(arg) < 2:
+            print ("** class name missing **")
+        elif len(arg) < 3:
+            print ("** instance id missing **")
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id"""
+        if len(arg) < 2:
+            print ("** class name missing **")
+        elif len(arg) < 3:
+            print ("** instance id missing **")
+    def do_all(self, arg):
+        """Prints all string representation of all
+        instances based or not on the class name"""
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id"""
+        if len(arg) < 2:
+            print ("** class name missing **")
+        elif len(arg) < 3:
+            print ("** instance id missing **")
+        elif len(arg) < 4:
+            print ("")
     def emptyline(self):
         return None
 
