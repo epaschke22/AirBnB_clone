0e631d7d204a994019816c0da27a831bcc549fc7
epaschke22 <ericksonpaschke@gmail.com>
2021-02-17 21:19:24 +0000

    updating file storage unit test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -13,14 +13,14 @@ class TestFileStorage(unittest.TestCase):
     def setUp(self):
         """runs before each test"""
         FileStorage._FileStorage__objects = {}
-        with open(FileStorage._FileStorage__file_path, 'w', encoding="utf-8") as file:
-            json.dump(output, file)
+        """with open(FileStorage._FileStorage__file_path, 'w', encoding="utf-8") as file:
+            json.dump(output, file)"""
     """def test_check_attributes(self):
-        """checks if attributes are correct"""
+        checks if attributes are correct
         test_storage = FileStorage()
         self.assertEqual(type(test_storage._FileStorage__objects), dict)
-        self.assertEqual(type(test_storage._FileStorage__file_path), str)
-    def test_filestroage_all(self):"""
+        self.assertEqual(type(test_storage._FileStorage__file_path), str)"""
+    def test_filestroage_all(self):
         """creating and testing objects dictionary"""
         self.maxDiff = None
         _obj = {}
